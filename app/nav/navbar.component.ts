fd98b4e5e020e2b8a9ebdb3792d227d32f2b4443
Brian Powers <brianpowers27@gmail.com>
2018-02-05 15:29:13 -0500

    Initial commit

---
+++ app/nav/navbar.component.ts
@@ -0,0 +1,13 @@
+import {Component } from '@angular/core'
+
+@Component({
+    selector:'nav-bar',
+    templateUrl:'app/nav/navbar.component.html',
+    styles:[`
+        .nav.navbar-nav {font-size: 15px}
+    `]
+})
+
+export class NavBarComponent{
+
+}
